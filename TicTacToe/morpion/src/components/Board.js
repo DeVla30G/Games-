3ea9bceebed9c0532c482043c123575a608cf3a5
DeVla30G <vladigrosjean@gmail.com>
2022-08-26 16:01:09 +0200

    features

--- TicTacToe/morpion/src/components/Board.js
+++ TicTacToe/morpion/src/components/Board.js
@@ -1,22 +1,44 @@
-import React from 'react';
-import Square from '../components/Square';
+import React, { Component } from 'react';
+import Square from './Square';
 
-
-const generateSquareComponents = (squares, onClickCallback) => {
-  // Complete this for Wave 1
-  // squares is a 2D Array, but 
-  //  you need to return a 1D array
-  //  of square components
-
-}
-
-const Board = ({ squares, onClickCallback }) => {
-  const squareList = generateSquareComponents(squares, onClickCallback);
-  console.log(squareList);
-  return <div className="grid" >
-    {squareList}
-  </div>
-}
-
-
-export default Board;
+class Board extends React.Component {
+    createBoard(row, col) {
+      const board = [];
+      let cellCounter = 0;
+  
+      for (let i = 0; i < row; i += 1) {
+        const columns = [];
+        for (let j = 0; j < col; j += 1) {
+          columns.push(this.renderSquare(cellCounter++));
+        }
+        board.push(<div key={i} className="board-row">{columns}</div>);
+      }
+  
+      return board;
+    }
+  
+    renderSquare(i) {
+      const winnerClass =
+        this.props.winnerSquares &&
+        (this.props.winnerSquares[0] === i ||
+          this.props.winnerSquares[1] === i ||
+          this.props.winnerSquares[2] === i)
+          ? 'square--green'
+          : '';
+  
+      return (
+        <Square
+          winnerClass={winnerClass}
+          key={i}
+          value={this.props.squares[i]}
+          onClick={() => this.props.onClick(i)}
+        />
+      );
+    }
+  
+    render() {
+      return <div>{this.createBoard(3, 3)}</div>;
+    }
+  }
+  
+  export default Board;
